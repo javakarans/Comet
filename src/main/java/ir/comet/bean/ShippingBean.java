43d90acdc169de43506c9b44f6f4893c8e53d260
Mohammad <javakarans@gmail.com>
2017-04-16 15:21:24 +0430

    shipping as Mohammad

---
+++ src/main/java/ir/comet/bean/ShippingBean.java
@@ -0,0 +1,14 @@
+package ir.comet.bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ * Created by Mohammad on 4/16/2017.
+ */
+@ManagedBean
+@ViewScoped
+public class ShippingBean {
+
+
+}
