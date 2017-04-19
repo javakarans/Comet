5c6ab2751835cee695eff3bbb019bba2988612a1
amir <am.m.noori@gmail.com>
2017-04-19 18:23:44 +0430

    admin update

--- src/main/java/ir/comet/controller/LoginController.java
+++ src/main/java/ir/comet/controller/LoginController.java
@@ -3,6 +3,9 @@ package ir.comet.controller;
 import ir.comet.database.CustomerDaoImp;
 import ir.comet.model.Customer;
 
+import javax.faces.context.FacesContext;
+import java.io.IOException;
+
 /**
  * Created by Mohammad on 4/13/2017.
  */
@@ -13,6 +16,14 @@ public class LoginController {
 
     public Customer customerLogin(){
 
+        if(userName.equals("admin"))
+        {
+            try {
+                FacesContext.getCurrentInstance().getExternalContext().redirect("../admin/adminPage.xhtml");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
         CustomerDaoImp customerDaoImp=new CustomerDaoImp();
         Customer customer= customerDaoImp.getCustomerByUserNameAndPass(this.userName,this.password);
         return customer;
