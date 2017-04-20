c2c280be6382bbed6ec29761b23cf0f1d3f66f95
Mohammad <javakarans@gmail.com>
2017-04-20 16:06:54 +0430

    UserAccount as Mohammad

--- src/main/java/ir/comet/bean/UserAccountBean.java
+++ src/main/java/ir/comet/bean/UserAccountBean.java
@@ -1,11 +1,14 @@
 package ir.comet.bean;
 
 import ir.comet.model.Customer;
+import ir.comet.model.Order;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Mohammad on 4/13/2017.
@@ -14,25 +17,5 @@ import javax.faces.context.FacesContext;
 @ViewScoped
 public class UserAccountBean {
 
-//    private Customer customer;
-//
-//    @PostConstruct
-//    public void init(){
-//        setCustomer(new Customer());
-//        loadUserSession();
-//    }
-//
-//    public void loadUserSession() {
-//        Customer customer = (Customer) FacesContext.getCurrentInstance().getExternalContext()
-//                .getSessionMap().get("customer");
-//        this.setCustomer(customer);
-//    }
-//
-//    public Customer getCustomer() {
-//        return customer;
-//    }
-//
-//    public void setCustomer(Customer customer) {
-//        this.customer = customer;
-//    }
+
 }
