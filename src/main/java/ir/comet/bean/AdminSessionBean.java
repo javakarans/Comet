e88e0701e635923373bd48fa9b6839dcb503766e
amir <am.m.noori@gmail.com>
2017-05-24 16:39:54 +0430

    update admin

---
+++ src/main/java/ir/comet/bean/AdminSessionBean.java
@@ -0,0 +1,20 @@
+package ir.comet.bean;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ * Created by amir on 5/24/2017.
+ */
+
+@ManagedBean
+@SessionScoped
+public class AdminSessionBean {
+
+    @PostConstruct
+    private void init()
+    {
+        
+    }
+}
