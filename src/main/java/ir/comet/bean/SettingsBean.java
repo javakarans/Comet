c552f7256bfdceb36241ae34a3daffa4fd0e9428
amir <am.m.noori@gmail.com>
2017-05-24 15:59:44 +0430

    update admin

---
+++ src/main/java/ir/comet/bean/SettingsBean.java
@@ -0,0 +1,19 @@
+package ir.comet.bean;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ * Created by amir on 5/24/2017.
+ */
+@ManagedBean
+@ViewScoped
+public class SettingsBean {
+
+    @PostConstruct
+    public void init()
+    {
+
+    }
+}
