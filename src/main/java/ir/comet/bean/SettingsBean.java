00bb936575534dc1e039bcf10dbd7c8d2991c5a0
amir <am.m.noori@gmail.com>
2017-05-24 18:49:58 +0430

    update admin

--- src/main/java/ir/comet/bean/SettingsBean.java
+++ src/main/java/ir/comet/bean/SettingsBean.java
@@ -1,5 +1,8 @@
 package ir.comet.bean;
 
+import ir.comet.model.Settings;
+import ir.comet.model.SettingsData;
+
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
@@ -11,9 +14,66 @@ import javax.faces.bean.ViewScoped;
 @ViewScoped
 public class SettingsBean {
 
+    private SettingsData settingsData;
+    private Settings settings;
+    private String currentPassWord;
+    private String firstNewPassWord;
+    private String secondNewPassWord;
+
+
     @PostConstruct
     public void init()
     {
+        settingsData = SettingsData.getInstance();
+        settings = settingsData.getSettings();
+    }
 
+    public void changePassword()
+    {
+        if (currentPassWord.equals(settings.getAdminPassword()))
+        {
+            if (firstNewPassWord.equals(secondNewPassWord))
+            {
+                settings.setAdminPassword(firstNewPassWord);
+                settingsData.updateSettings(settings);
+            }
+        }
+    }
+
+    public void changeUserName()
+    {
+        settingsData.updateSettings(settings);
+    }
+
+    public Settings getSettings() {
+        return settings;
+    }
+
+    public void setSettings(Settings settings) {
+        this.settings = settings;
+    }
+
+    public String getCurrentPassWord() {
+        return currentPassWord;
+    }
+
+    public void setCurrentPassWord(String currentPassWord) {
+        this.currentPassWord = currentPassWord;
+    }
+
+    public String getFirstNewPassWord() {
+        return firstNewPassWord;
+    }
+
+    public void setFirstNewPassWord(String firstNewPassWord) {
+        this.firstNewPassWord = firstNewPassWord;
+    }
+
+    public String getSecondNewPassWord() {
+        return secondNewPassWord;
+    }
+
+    public void setSecondNewPassWord(String secondNewPassWord) {
+        this.secondNewPassWord = secondNewPassWord;
     }
 }
