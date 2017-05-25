ec95a266c3392707a7b775161266d5fcb191f724
Mohammad <javakarans@gmail.com>
2017-05-25 04:41:57 +0430

    Update

--- src/main/java/ir/comet/Test.java
+++ src/main/java/ir/comet/Test.java
@@ -4,10 +4,17 @@ import ir.comet.Utilities.TrackNumber;
 import ir.comet.database.BranchDao;
 import ir.comet.database.BranchDaoImp;
 import ir.comet.database.CustomerDaoImp;
+import ir.comet.database.HibernateUtil;
 import ir.comet.model.Branch;
 import ir.comet.model.Customer;
+import ir.comet.model.Product;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Projection;
+import org.hibernate.criterion.Projections;
 
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 import java.util.UUID;
 
@@ -17,17 +24,17 @@ import java.util.UUID;
 public class Test {
 
     public static void main(String[] args) {
-//        Customer customer=new Customer();
-//        customer.setEmail("good");
-//        CustomerDaoImp customerDaoImp=new CustomerDaoImp();
-//        customerDaoImp.createCustomer(customer);
 
-        BranchDaoImp branchDaoImp = new BranchDaoImp();
-        List<Branch> branches = branchDaoImp.getBranchesByCategoryId(4);
-        for (Branch branch: branches
-             ) {
-            System.out.println(branch.getBranchName());
-
-        }
+        HibernateUtil.beginTransaction();
+        Session session = HibernateUtil.getSession();
+        Criteria criteria = session.createCriteria(Product.class);
+        long id= (long) criteria.setProjection(Projections.max("price")).uniqueResult();
+        System.out.println(id);
+//        List<Product> list = criteria.list();
+//        Iterator<Product> iterator = list.iterator();
+//        while (iterator.hasNext()){
+//            System.out.println(iterator.next().getProductId());
+//        }
     }
+
 }
